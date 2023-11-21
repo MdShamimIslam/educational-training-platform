19f506ea1eab9e2910e324a03968351645dea8b3
MdShamimIslam <shamim401897@gmail.com>
2023-11-21 15:47:55 +0600

    tailwind react-router-dom and daisyui addeded

---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../assets/logo.png";
+
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="mt-8">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navLinks}
+            </ul>
+          </div>
+          <Link to="/" className="btn btn-ghost text-xl">
+            <img className="w-6" src={logo} alt="website-logo" />
+            <span className="text-3xl font-bold">s.Academy</span>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
